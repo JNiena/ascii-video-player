d15920e7cdc2aecce1e7c09b0d6ab4cc020c758e
JNiena <JNiena@protonmail.com>
2022-07-01 04:19:48 -0400

    Update ascii_player.hpp

--- ascii_player.hpp
+++ ascii_player.hpp
@@ -11,11 +11,15 @@ namespace cv {
 
 class AsciiPlayer {
   public:
-    const void stream(const std::function<void(const std::string& frame)> callback, const std::string& path, const unsigned int width, const unsigned int height) const;
+    const void play(const std::string& path, const unsigned int width, const unsigned int height, const bool color, const bool audio) const;
+  private:
+    const void stream_colorless(const std::function<void(const std::string& frame)> callback, const std::string& path, const unsigned int width, const unsigned int height) const;
 
     const void stream_color(const std::function<void(const std::string& frame)> callback, const std::string& path, const unsigned int width, const unsigned int height) const;
-  private:
-    const std::string frame_to_ascii(const cv::Mat& frame) const;
+
+    const void stream(const std::function<void(const std::string& frame)> callback, const std::string& path, const unsigned int width, const unsigned int height, const bool color) const;
+
+    const std::string frame_to_ascii_colorless(const cv::Mat& frame) const;
 
     const std::string frame_to_ascii_color(const cv::Mat& frame) const;
 
@@ -27,6 +31,8 @@ class AsciiPlayer {
     const std::string calculate_pixel_density(const std::vector<std::string>& densities, const unsigned int value) const;
 
     const void play_audio(const std::string& path) const;
+
+    const unsigned int find_framerate(const std::string& path) const;
 };
 
 #endif
