401825f411ec8f69c98f34c14e0b5f82d3074878
JNiena <JNiena@protonmail.com>
2022-07-03 23:56:29 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,19 +7,28 @@ int main(int argc, char **argv) {
   std::string path = "";
   unsigned int width = 0;
   unsigned int height = 0;
+  unsigned int framerate = 0;
   bool color = false;
   bool audio = false;
+  bool fill = false;
+  bool preprocess = false;
   lyra::cli parser = lyra::cli()
     | lyra::opt(path, "path") ["-p"]["--path"]("The path to the video.").required()
-    | lyra::opt(width, "width") ["-w"]["--width"]("The display width.").required()
-    | lyra::opt(height, "height") ["-h"]["--height"]("The display height.").required()
-    | lyra::opt(color, "color") ["-c"]["--color"]("Display color.").optional()
-    | lyra::opt(audio, "audio") ["-a"]["--audio"]("Play audio.").optional();
+    | lyra::opt(width, "width") ["-w"]["--width"]("The display width.").optional()
+    | lyra::opt(height, "height") ["-h"]["--height"]("The display height.").optional()
+    | lyra::opt(framerate, "framerate") ["-f"]["--framerate"]("The framerate of the video.").optional()
+    | lyra::opt(color) ["-c"]["--color"]("Display color.").optional()
+    | lyra::opt(audio) ["-a"]["--audio"]("Play audio.").optional()
+    | lyra::opt(fill) ["--fill"]("Fills the display.").optional();
   lyra::parse_result result = parser.parse({argc, argv});
   if (result) {
     if (!std::filesystem::exists(path)) {
       fmt::print("The file has not been found.\n");
       return -1;
+    }
+    if (fill) {
+      AsciiPlayer().play(path, color, audio, framerate);
+      return 0;
     }
     if (!(width > 0)) {
       fmt::print("The width must be greater than zero.\n");
@@ -29,7 +38,7 @@ int main(int argc, char **argv) {
       fmt::print("The height must be greater than zero.\n");
       return -1;
     }
-    AsciiPlayer().play(path, width, height, color, audio);
+    AsciiPlayer().play(path, width, height, color, audio, framerate);
   } else {
     fmt::print("{}\n", result.message());
   }
